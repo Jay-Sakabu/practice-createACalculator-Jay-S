43d8ef1a1fe2b4feabd140f4c4355f9a0cc206d3
Jay S <js4cap@gmail.com>
2025-04-22 11:04:31 -0500

    Function corrections

--- calc.js
+++ calc.js
@@ -15,9 +15,12 @@ function getSqrt(number)
     return Math.sqrt(number);
 }
 
-function getMin(numberArray)
+function getMinMax(numberArray)
 {
-    return Math.min(...numberArray);
+    return {
+        max: Math.min(...numberArray),
+        min: Math.max(...numberArray)
+    };
 }
 
 function generateRandomNumber(min, max)
@@ -25,9 +28,9 @@ function generateRandomNumber(min, max)
     return Math.floor(Math.random() * (max - min) + min); // Generate random number
 }
 
-function roundToTwoPlaces(number)
+function roundToDecimalPlaces(number, decimal)
 {
-    return Math.round(number * 100)/100;
+    return Number(number.toFixed(decimal));
 }
 
 //Function Tests
@@ -36,6 +39,8 @@ console.log(`5 rasied to the power of 3: ${raisePow(5,3)}`);
 console.log(`Square root of 144: ${getSqrt(144)}`);
 
 jArray = [3, 78, -12, 0.5, 27];
-console.log(`Min of array ${jArray}: ${getMin(jArray)}`);
+const {max, min} = getMinMax(jArray)
+console.log(`Min of array: ${min}
+Max of array: ${max}`);
 console.log(`Random number between 1 and 10 (inclusive): ${generateRandomNumber(1,10)}`);
-console.log(`12.3456789 rounded to two places: ${roundToTwoPlaces(12.3456789)}`);
+console.log(`12.3456789 rounded to two places: ${roundToDecimalPlaces(12.3456789, 2)}`);
